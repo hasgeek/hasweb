cfba3b6846abcce81dae4488c2510ade868c9365
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-02-15 15:46:18 +0530

    Flatpages config.

---
+++ instance/settings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#: Site title
+SITE_TITLE = 'HasGeek'
+#: Site id (for network bar)
+SITE_ID = 'home'
+#: Google Analytics code
+GA_CODE = 'UA-19123154-1'
+#: Timezone
+TIMEZONE = 'Asia/Calcutta'
+#: Flat pages
+FLATPAGES_AUTO_RELOAD = False
+FLATPAGES_EXTENSION = '.md'
+#: LastUser server
+LASTUSER_SERVER = 'https://auth.hasgeek.com/'
+MAIL_FAIL_SILENTLY = False
+MAIL_SERVER = 'localhost'
+DEFAULT_MAIL_SENDER = ('HasGeek', 'bot@hasgeek.com')
+#: Logging: recipients of error emails
+ADMINS = []
+#: Log file
+LOGFILE = 'error.log'
